ce6d9cf573db722c142354fefc47c1c18c2d0264
Faysal Ahmed <kawsar.ahmed042@gmail.com>
2024-08-15 00:23:19 +0400

    added icons

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ import CreateTournamentPopup from "./CreateTournament";
 import UpdateScorePopup from "./UpdateScore";
 import Notification from "./Notification";
 import Footballl from "./football.svg";
+import Shoe from "./shoe.svg";
+import Cup from "./cup.svg";
 import MatchTabs from "./MatchTab";
 
 const App = () => {
@@ -311,22 +313,22 @@ const App = () => {
                     setSelectedTournament(tournament);
                   }}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="w-5 h-5 mr-3"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M8 21h8M12 17v4M7 4h10v8a5 5 0 01-10 0V4zm12 0v4a2 2 0 01-2 2M5 4v4a2 2 0 002 2"
+                  {tournament.winner ? (
+                    <img
+                      src={Cup}
+                      alt="Football Icon"
+                      className="w-5 h-5 mr-3"
                     />
-                  </svg>
+                  ) : (
+                    <img
+                      src={Shoe}
+                      alt="Football Icon"
+                      className="w-5 h-5 mr-3"
+                    />
+                  )}
 
                   {tournament.name}
+                  {tournament.winner && ` - ${tournament.winner}`}
                 </li>
               ))}
             </ul>
@@ -342,10 +344,7 @@ const App = () => {
             selectedTournament && (
               <div>
                 <h2 className="text-xl font-semibold mb-4">
-                  Matches for {selectedTournament.name}{" "}
-                  {selectedTournament.winner
-                    ? `Winner - ${selectedTournament.winner}`
-                    : ""}
+                  Matches for {selectedTournament.name}
                 </h2>
 
                 <div className="grid">
